6c0ba73aec7f1e942134918ca3e1c01d73b169ef
luk355 <luk355@gmail.com>
2016-10-13 14:13:01 +1300

    Removed setters from domain event interfaces

--- src/UnifiedMediatR/Eventing/IDomainEvent.cs
+++ src/UnifiedMediatR/Eventing/IDomainEvent.cs
@@ -3,24 +3,24 @@ using MediatR;
 
 namespace UnifiedMediatR.Eventing
 {
-    public interface IDomainEvent<TAggregateId> : INotification
+    public interface IDomainEvent<out TAggregateId> : INotification
     {
         /// <summary>
         /// The Aggregate Id of the Aggregate root that the domain concerns.
         /// </summary>
-        TAggregateId Id { get; set; }
+        TAggregateId Id { get; }
     }
 
     public interface IDomainEventHandler<in TEvent, TAggregateId> : INotificationHandler<TEvent> where TEvent : IDomainEvent<TAggregateId>
     {
     }
 
-    public interface IAsyncDomainEvent<TAggregateId> : IAsyncNotification
+    public interface IAsyncDomainEvent<out TAggregateId> : IAsyncNotification
     {
         /// <summary>
         /// The Aggregate Id of the Aggregate root that the domain concerns.
         /// </summary>
-        TAggregateId Id { get; set; }
+        TAggregateId Id { get; }
     }
 
     public interface IAsyncDomainEventHandler<in TEvent, TAgrregateId> : IAsyncNotificationHandler<TEvent> where TEvent : IAsyncDomainEvent<TAgrregateId>
